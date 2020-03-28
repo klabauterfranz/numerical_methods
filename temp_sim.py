d9f67670debec0b5d51e7a50d020d100442b31cb
David Zhou <d.zhou@posteo.at>
2020-03-28 08:50:52 +0100

    First Commit

---
+++ temp_sim.py
@@ -0,0 +1,112 @@
+import numpy as np
+import time
+from numba import njit, prange
+import tqdm
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('{} - {} ms'.format(method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+
+@timeit
+def tempeartureprofile(position=None, limit=45, delta_x=0.01, delta_t=0.1, time=360.0, length=0.4, **parameters):
+	"""
+	Lösung der fourierischen Wärmeleitgleichung
+	ermittlung des Zeitpunktes bei Überschreitung einer bestimmten Temperatur an einer bestimmten Position
+	
+	:param position: (int) Parameter für die Ausgabe der Zeit an einer bestimmten Stelle [pos] = m {Standardwert None}
+	:param limit: (float) Grenztemperatur [limit] = °C {Standardwert 45}
+	:param delta_x: (float) Schritte für die Ortsdiskretisierung [Δx] = m {Standardwert 0.01}
+	:param delta_t: (float) Schritte für die Zeitdiskretisierung [Δt] = s {Standardwert 0.01}
+	:param time: (float) Dauer der Simulation [t] = s {Standardwert 0.01}
+	:param length: (float) Länge des Stabes [l] = m {Standardwert 0.4}
+	:param parameters: (Dictionary) die für die Berechnung relevanten Parameter
+	"""
+	
+	par_c = parameters['c']
+	par_rho = parameters['rho']
+	par_lambda = parameters['lambda']
+	par_alpha = parameters['alpha']
+	par_r = parameters['radius']
+	par_a = par_lambda/par_c/par_rho
+	
+	temp_boundary = parameters['temp_boundary']
+	temp_init = parameters['temp_init']
+	
+	# Stabilitätskriterium nach Krebs et al. 
+	if delta_t != 0.1:
+		delta_x = (2*par_a*delta_t)**(1/2)
+	if delta_x != 0.01:
+		delta_t = (delta_x**2/par_a)
+	
+	time_steps = (int) (time/delta_t) + 1
+	location_steps = (int) (length/delta_x) + 1
+	
+	A = par_lambda * delta_t / par_c / par_rho / delta_x**2
+	B = 2 * par_alpha * delta_t / par_c / par_rho / par_r
+	
+	# Anfangstemperaturprofil
+	temp_profile = np.asarray([np.insert(np.full(location_steps-1, temp_boundary), 0, temp_init)])
+	
+	# Iteration über die Zeitpunkte
+	for time in tqdm.trange(time_steps):
+		t1 = np.delete(temp_profile[time], 0)
+		t2 = np.delete(t1, 0)
+		t0 = np.resize(temp_profile[time], t2.size)
+		t1 = np.resize(t1, t2.size)
+		
+		last = t0[-1] + (t0[-2]-t0[-1]) * A - B * (t0[-1]-t0[-2])
+		tnkp1 = t1 + (- 2*t1 + t0 + t2) * A - B * (t1-temp_boundary)
+		tnkp1 = np.append(np.insert(tnkp1, 0, temp_init), last)
+		temp_profile=np.vstack([temp_profile, tnkp1])
+	
+	#Speichern des Temperaturprofils als CSV
+	#np.savetxt("temp_profile.csv", temp_profile, delimiter=";")
+	
+	time = find_time(temp_profile=temp_profile, limit=limit, position=position, delta_t=delta_t, delta_x=delta_x, length=length)
+	return time
+
+
+def find_time(temp_profile, limit, position, delta_t, delta_x, length):
+	temp_profile_T = np.transpose(temp_profile)
+	time_cum_sum = np.insert(np.cumsum(np.full(len(temp_profile_T[0])-1, delta_t)),0,0) # Liste der Zeitpunkte
+	time_gt = [np.interp(limit, temp_profile_T[i], time_cum_sum) for i in range(len(temp_profile_T))] # Linear Interpolierte Zeit
+	
+	if position is not None:
+		#location_cumsum = np.insert(np.cumsum(np.full(len(temp_profile[0])-1, delta_x)),0,0)
+		location_cumsum =  np.resize(np.arange(0.005, length+delta_x, delta_x),len(time_gt))
+		print(len(location_cumsum))
+		time = np.interp(position, location_cumsum, time_gt)
+		return time
+	else:
+		return time_gt
+
+	
+
+if __name__=="__main__":
+	parameters = {
+				'c' : 888,
+				'rho' : 2700,
+				'lambda' : 237,
+				'alpha' : 8.000,
+				'radius' : 0.003,
+				'temp_boundary' : 22.00, 
+				'temp_init' : 300}
+	
+	
+	#TESTS
+	print("| Starte Kalkulation mit Parametern |")
+	time_gt = tempeartureprofile(**parameters)
+	print(time_gt)
+	
+	print("| Starte Kalkulation mit Position |")
+	position = tempeartureprofile(position =0.075, **parameters)
+	print("An der Position 0.075 m wird die Grenztemperatur nach {} s erreicht".format(round(position,3)))
